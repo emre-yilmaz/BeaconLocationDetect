13701cfbfa8973dc571ffb17488d2883660b0b87
Emre YILMAZ <emreyillmaz@gmail.com>
2017-04-26 10:46:10 +0300

    first commit

---
+++ LokasyonBeacon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LokasyonBeacon
+//
+//  Created by Elmas Mac on 24/04/2017.
+//  Copyright © 2017 Emre YILMAZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
